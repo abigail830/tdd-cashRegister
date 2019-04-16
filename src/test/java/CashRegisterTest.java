864ea6f1f0dd7106176ff35cf9ba827368c44980
SaraQian <abigail830@163.com>
2019-04-16 14:50:56 +0800

    step2. using Mokito

--- src/test/java/CashRegisterTest.java
+++ src/test/java/CashRegisterTest.java
@@ -1,21 +1,80 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import java.util.Arrays;
 
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+
 class CashRegisterTest {
 
+    @Mock
+    Purchase globalMockPurchase;
+    @Mock
+    Printer globalMockPrinter;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+    }
+
     @Test
-    void should_able_to_invoke_printer_with_right_content_with_mockprinter() {
+    void should_able_to_invoke_printer_with_right_content_with_manual_mockprinter() {
         //given
         final Item item = new Item("Name", 100.0);
         Purchase purchase = new Purchase(Arrays.asList(item));
-        final MockPrinter printer = new MockPrinter();
+        final MockPrinter mockPrinter = new MockPrinter();
+        final CashRegister cashRegister = new CashRegister(mockPrinter);
+
+        //when
+        cashRegister.process(purchase);
+
+        //then
+        mockPrinter.verifyPrinted("Name\t100.0\n");
+    }
+
+    @Test
+    void should_able_to_invoke_printer_with_right_content_with_mokito_mockprinter() {
+        //given
+        final Item item = new Item("Name", 100.0);
+        Purchase purchase = new Purchase(Arrays.asList(item));
+        Printer printer = mock(Printer.class);
         final CashRegister cashRegister = new CashRegister(printer);
 
         //when
         cashRegister.process(purchase);
 
         //then
-        printer.verifyPrinted("Name\t100.0\n");
+        verify(printer).print("Name\t100.0\n");
     }
+
+    @Test
+    void should_able_to_invoke_printer_with_right_content_with_mokito_mockprinter_and_mockpurchase() {
+        //given
+        Purchase purchase = mock(Purchase.class);
+        when(purchase.getDescription()).thenReturn("Purchase Description");
+        Printer printer = mock(Printer.class);
+        final CashRegister cashRegister = new CashRegister(printer);
+
+        //when
+        cashRegister.process(purchase);
+
+        //then
+        verify(printer).print("Purchase Description");
+    }
+
+    @Test
+    void should_able_to_invoke_printer_with_right_content_with_another_way_of_mock() {
+        //given
+        when(globalMockPurchase.getDescription()).thenReturn("Purchase Description");
+        final CashRegister cashRegister = new CashRegister(globalMockPrinter);
+
+        //when
+        cashRegister.process(globalMockPurchase);
+
+        //then
+        verify(globalMockPrinter).print("Purchase Description");
+    }
+
 }
